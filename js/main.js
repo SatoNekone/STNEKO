19e1212f555c48f466924065f082f79245debf0d
SatoshiNekos <dacapo286@gmail.com>
2016-11-10 04:43:23 +0800

    0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).load(function() {
-    $('.bg').parallax("50%", 0.4);
+    $('.body').parallax("50%", 0.4);
     $('.bg1').parallax("50%", 0.4);
     $('.bg2').parallax("50%", 0.4); //jQuery selector中可以放任何你想要parallax效果的元素
 }
